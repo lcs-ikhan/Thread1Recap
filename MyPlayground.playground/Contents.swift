db854af77c0324c2a0dc32eed6082c6e81281472
Isaad Khan <IKhan@lcs.on.ca>
2023-01-10 14:55:53 -0500

    Added computed properties

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,13 +1,27 @@
 import Cocoa
 
 struct Dog {
-    let breed: String
+    //MARK: Stored properties
+        let breed: String
         let color: String
         var heightInMetres: Double
-        var lengthInMeters: Double
+        var lengthInMetres: Double
         var massInPounds: Double
         var ageInYears: Double
         var birthday: String
+    
+    //MARK: Computed properties
+    var heightInCentimetres: Double {
+        return heightInMetres * 100
+    }
+    var lengthInCetimetres: Double {
+        lengthInMetres * 100
+    }
+    var ratioOfLengthHeight: Double{
+        lengthInMetres * heightInMetres
+    }
 }
 
+struct myDog = Dog(breed: "German Shephard", color: "Yellowish Oragne", heightInMetres: 3, lengthInMetres: 1.5, massInPounds: 200, ageInYears: 13, birthday: "Feb 17")
 
+myDog.heightInMeters
