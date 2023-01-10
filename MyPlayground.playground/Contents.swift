599ad8bc16a730a3b82e7cbb925afece3ff07d16
Isaad Khan <IKhan@lcs.on.ca>
2023-01-10 14:38:29 -0500

    Added other properties

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -4,5 +4,10 @@ struct Dog {
     let breed: String
         let color: String
         var heightInMetres: Double
+        var lengthInMeters: Double
+        var massInPounds: Double
+        var ageInYears: Double
+        var birthday: String
 }
+
 
